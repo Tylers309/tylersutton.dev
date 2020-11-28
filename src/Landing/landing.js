a3656a3ba613632d5c4759cf502b9e8dfe0ccd46
Tyler Sutton <309jtsutton@gmail.com>
2020-11-27 19:34:36 -0600

    remove nav until permenant solution implemented

--- src/Landing/landing.js
+++ src/Landing/landing.js
@@ -9,7 +9,7 @@ class Landing extends Component {
   render() {
     return (
       <Wrapper>
-        <Navigation />
+        {/* <Navigation /> */}
         <Triangles />
         <WelcomeHeader />
         <CodeBox />
